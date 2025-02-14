b9f8ed3871e097c2ec99a1df4de41d7dcf3a2bd9
a_little_mayhem <parabola657@gmail.com>
2025-02-14 16:25:39 +0300

    Fixed go_to_element in BasePage; update elements_test; added test for checkboxes

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,7 +1,11 @@
+import random
 import time
+from typing import List
+
+from selenium.webdriver.remote.webelement import WebElement
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
 from pages.base_page import BasePage
 
 
@@ -10,10 +14,10 @@ class TextBoxPage(BasePage):
 
     def fill_all_fields(self) -> tuple[str, str, str, str]:
         person_info = next(generated_person())
-        full_name = person_info.full_name
-        email = person_info.email
-        current_address = person_info.current_address
-        permanent_address = person_info.permanent_address
+        full_name: str = person_info.full_name
+        email: str = person_info.email
+        current_address: str = person_info.current_address
+        permanent_address: str = person_info.permanent_address
 
         self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
         self.element_is_visible(self.locators.EMAIL).send_keys(email)
@@ -28,3 +32,36 @@ class TextBoxPage(BasePage):
         current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
         permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
+
+class CheckBoxPage(BasePage):
+    locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_checkbox(self):
+        item_list: List = self.elements_are_visible(self.locators.ITEMS_LIST)
+        count: int = 21
+        while count != 0:
+            item = item_list[random.randint(1, 15)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                count -= 1
+            else:
+                break
+
+    def get_checked_checkboxes(self) -> List[str]:
+        checked_list : List[WebElement] = self.elements_are_present(self.locators.CHECKED_ITEMS)
+        data: List[str] = []
+        for box in checked_list:
+            title_item = box.find_element("xpath" ,self.locators.TITLE_ITEM)
+            data.append(title_item.text.replace(' ', '').replace('.doc', '').lower())
+        return data
+
+    def get_output_result(self):
+        result_list: List[WebElement] = self.elements_are_present(self.locators.OUTPUT_RESULT)
+        data: List[str] = []
+        for item in result_list:
+            data.append(item.text.lower())
+        return data
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,9 @@
 import time
+from typing import List
 
-from pages.elements_page import TextBoxPage
+from selenium.webdriver.remote.webelement import WebElement
+
+from pages.elements_page import TextBoxPage, CheckBoxPage
 from conftest import driver
 
 
@@ -17,3 +20,14 @@ class TestElements:
             assert email == output_email, "Email does not match"
             assert current_address == output_cur_addr, "Current address does not match"
             assert permanent_address == output_perm_addr, "Permanent address does not match"
+
+    class TestCheckBox:
+        def test_checkbox(self, driver):
+            checkbox_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            checkbox_page.open()
+            checkbox_page.open_full_list()
+            checkbox_page.click_random_checkbox()
+            input_checkbox: List[str] = checkbox_page.get_checked_checkboxes()
+            output_result: List[str] = checkbox_page.get_output_result()
+
+            assert input_checkbox == output_result, "Checkboxes have not been selected "
